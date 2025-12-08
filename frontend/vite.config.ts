f9ba09e6db293591e968c35ff48ebf426aed9cf0
BATTU RISHITHA <22h51a6610@cmrcet.ac.in>
2025-12-08 19:49:57 +0530

    installed tailwind

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+// This plugin allows Tailwind CSS to work smoothly inside a Vite project.
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tailwindcss()],
+});
+//It enables Tailwind inside Vite
